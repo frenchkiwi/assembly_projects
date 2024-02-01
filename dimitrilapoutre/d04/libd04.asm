bb35e06b00d2bffaaa0978a0f2f741ce253f8bff
DimitriLaPoudre <lou.pellegrino@epitech.eu>
2024-02-01 12:52:59 +0100

    [+][DLP] update lib

--- dimitrilapoutre/d04/libd04.asm
+++ dimitrilapoutre/d04/libd04.asm
@@ -97,3 +97,30 @@ my_getnbr:
         neg rax
         bye2:
             ret
+
+my_sort_int_array:
+    mov rcx, 0
+    loop_sort_int_array:
+        cmp rcx, rsi
+        je bye_sort_int_array
+        mov rdx, rcx
+        loop_sort_int_array2:
+            inc rdx
+            mov r8d, dword [rdi + rdx * 4]
+            cmp dword [rdi + rcx * 4], r8d
+            jg swap_sort_int_array
+            back_up_sort_int_array:
+                cmp rdx, rsi
+                jne loop_sort_int_array2
+        inc rcx
+        jmp loop_sort_int_array
+    
+    swap_sort_int_array:
+        mov r8d, dword [rdi + rcx * 4]
+        mov r9d, dword [rdi + rdx * 4]
+        mov dword [rdi + rcx * 4], r9d
+        mov dword [rdi + rdx * 4], r8d
+        jmp back_up_sort_int_array
+    
+    bye_sort_int_array:
+        ret
