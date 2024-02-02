6206112957f76f9263d01d14b2ed297fd93328e5
DimitriLaPoudre <lou.pellegrino@epitech.eu>
2024-02-02 13:18:01 +0100

    [+][DLP] my_is_prime

---
+++ dimitrilapoutre/d05/my_is_prime_sup.asm
@@ -0,0 +1,50 @@
+section .data
+
+section .text
+    global _start
+
+my_is_prime_sup:
+    cmp rdi, 1
+    jle zero_prime_sup
+
+    mov rax, rdi
+    xor rdx, rdx
+    mov rbx, 2
+    div rbx
+
+    cmp rdi, 2
+    je one_prime_sup
+
+    cmp rdx, 0
+    je zero_prime_sup
+
+    mov r8, rax
+    loop_prime_sup:
+        inc rbx
+        cmp rbx, r8
+        jge one_prime_sup
+
+        mov rax, rdi
+        xor rdx, rdx
+        div rbx
+
+        cmp rdx, 0
+        je zero_prime_sup
+        jmp loop_prime_sup
+
+    one_prime_sup:
+        mov rax, 1
+        ret
+    zero_prime_sup:
+        mov rax, 0
+        ret
+
+_start:
+    mov rdi, 31
+    call my_is_prime_sup
+    jmp _exit
+
+_exit:
+    mov rax, 60
+    mov rdi, 0
+    syscall
--- dimitrilapoutre/lib.asm
+++ dimitrilapoutre/lib.asm
@@ -235,3 +235,39 @@ my_compute_power_rec:
     neg_power_rec:
         mov rax, 0
         ret
+
+my_is_prime:
+    cmp rdi, 1
+    jle zero_prime
+
+    mov rax, rdi
+    xor rdx, rdx
+    mov rbx, 2
+    div rbx
+
+    cmp rdi, 2
+    je one_prime
+
+    cmp rdx, 0
+    je zero_prime
+
+    mov r8, rax
+    loop_prime:
+        inc rbx
+        cmp rbx, r8
+        jge one_prime
+
+        mov rax, rdi
+        xor rdx, rdx
+        div rbx
+
+        cmp rdx, 0
+        je zero_prime
+        jmp loop_prime
+
+    one_prime:
+        mov rax, 1
+        ret
+    zero_prime:
+        mov rax, 0
+        ret
