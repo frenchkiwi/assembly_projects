407d0c3dbd73eb279ec670d78d459bf4bc772de9
DimitriLaPoudre <lou.pellegrino@epitech.eu>
2024-02-04 06:29:33 +0100

    [+][DLP] my_showstr

--- dimitrilapoutre/lib.asm
+++ dimitrilapoutre/lib.asm
@@ -27,6 +27,7 @@ section .text
     global my_str_isprintable
     global my_putnbr_base
     global my_getnbr_base
+    global my_showstr
 
 my_swap:
     xchg rdi, rsi
@@ -303,7 +304,7 @@ my_is_prime:
         mov rax, 0
         ret
 
-my_is_prime_sup:
+my_find_prime_sup:
     cmp rdi, 1
     jle zero_prime_sup
 
@@ -337,7 +338,7 @@ my_is_prime_sup:
         ret
     zero_prime_sup:
         inc rdi
-        call my_is_prime_sup
+        call my_find_prime_sup
         ret
 
 my_strcpy:
@@ -779,3 +780,78 @@ my_getnbr_base:
         neg rax
         bye_getnbr_base2:
             ret
+
+my_showstr:
+    mov r8, rdi
+    mov r9, -1
+    loop_showstr:
+        inc r9
+        cmp byte [r8 + r9], 0
+        je bye_showstr
+        jmp check_showstr
+
+    bye_showstr:
+        mov rax, 0
+        ret
+
+    check_showstr:
+        cmp byte [r8 + r9], ' '
+        jl np_showstr
+        cmp byte [r8 + r9], '~'
+        jg np_showstr
+        jmp p_showstr
+
+    p_showstr:
+        mov rax, 1
+        mov rdi, 1
+        lea rsi, [r8 + r9]
+        mov rdx, 1
+        syscall
+        jmp loop_showstr
+
+    np_showstr:
+        mov rax, 1
+        mov rdi, 1
+        mov rdx, 1
+
+        push r8
+        mov byte [r8], '\' 
+        lea rsi, [r8]
+        syscall
+        pop r8
+        
+        movzx rax, byte [r8 + r9]
+        xor rdx, rdx
+        mov rbx, 16
+        div rbx
+
+        mov r10, rax
+        add r10, 48
+        mov r11, rdx
+        add r11, 48
+
+        mov rax, 1
+        mov rdi, 1
+        mov rdx, 1
+
+        push r8
+        call is_hexa_showstr
+        mov byte [r8], r10b
+        lea rsi, [r8]
+        mov r10, r11
+        syscall
+        call is_hexa_showstr
+        mov byte [r8], r10b
+        call is_hexa_showstr
+        lea rsi, [r8]
+        syscall
+        pop r8
+
+        jmp loop_showstr
+
+        is_hexa_showstr:
+            cmp r10, 58
+            jl bye_is_hexa_showstr
+            add r10, 39
+            bye_is_hexa_showstr:
+                ret
