33ddafba853a6d5b9db6fed68269681f11961c98
DimitriLaPoudre <lou.pellegrino@epitech.eu>
2024-06-19 23:42:20 +0200

    [+] AsmCoreMemory for store malloc_base

--- AsmLibrary/lib/AsmAlloc.asm
+++ AsmLibrary/lib/AsmAlloc.asm
@@ -1,9 +1,7 @@
 section .data
-    malloc_base dq -1 ; 8octet for next_malloc_page | 8octet free to use | 240 node of (8octet for size of allocation | 8octet for addr of allocation | 1octet for allocation state (1 = owned | 0 = free))
     malloc_bug db "AsmAlloc(): memory allocation failed", 10
 
 section .text
-    global malloc_base
     global AsmAlloc
     %include "AsmLibrary.inc"
 
---
+++ AsmLibrary/lib/AsmCoreMemory.asm
@@ -0,0 +1,6 @@
+section .data
+    malloc_base dq -1 ; 8octet for next_malloc_page | 8octet free to use | 240 node of (8octet for size of allocation | 8octet for addr of allocation | 1octet for allocation state (1 = owned | 0 = free))
+
+section .text
+    global malloc_base
+    %include "AsmLibrary.inc"
